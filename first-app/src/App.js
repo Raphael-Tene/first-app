8271e418b5e8ed9e3ab950f73a6206596433b7e2
Raphael <teneraphael@gmail.com>
2022-11-09 13:05:56 +0000

    added expense form

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,9 +1,11 @@
 import Expenses from './components/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 import expenses from './expensesData/expensesData';
 
 function App() {
   return (
     <div className="App">
+      <NewExpense />
       <Expenses expenses={expenses} />
 
     </div>
