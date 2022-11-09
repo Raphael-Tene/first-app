d330aa9de1e198f48f0bdd626254e978fda073e3
Raphael <teneraphael@gmail.com>
2022-11-09 04:14:55 +0000

    added expenses data and a card component

--- first-app/src/components/ExpenseItem.js
+++ first-app/src/components/ExpenseItem.js
@@ -1,21 +1,19 @@
+import Card from './Card';
+import DateItem from './DateItem';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2021, 2, 28).toDateString();
-    const expenseTitle = 'Car insurance';
-    const expenseAmount = 456.8;
 
     return (
-        <div className="expense-item">
-            <div>
-                {props.date.toDateString()}
-            </div>
+        <Card className="expense-item">
+            <DateItem date={props.date} />
             <div className="expense-item__description">
                 <h2 className='expense-item__h2'>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+        </Card>
 
-        </div>
+
     )
 };
 
